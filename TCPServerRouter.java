3574f9673188d4d92fab640855e6df5d5a6a2e31
Evan <evanvirani@gmail.com>
2021-12-04 11:14:06 -0500

    Update TCPServerRouter.java

--- TCPServerRouter.java
+++ TCPServerRouter.java
@@ -7,31 +7,31 @@ public class TCPServerRouter {
         Object [][] RoutingTable = new Object [10][2]; // routing table
         int SockNum = 5555; // port number
         Boolean Running = true;
-        int ind = 0; // index in the routing table
+        int ind = 0; // indext in the routing table
 
         //Accepting connections
         ServerSocket serverSocket = null; // server socket for accepting connections
         try {
-            serverSocket = new ServerSocket(SockNum);
-            System.out.println("| ServerRouter is Listening on port: " + SockNum + ".");
+            serverSocket = new ServerSocket(5555);
+            System.out.println("ServerRouter is Listening on port: 5555.");
         }
         catch (IOException e) {
-            System.err.println("| Could not listen on port: " + SockNum + ".\nReason: " + e.toString());
+            System.err.println("Could not listen on port: 5555.");
             System.exit(1);
         }
 
         // Creating threads with accepted connections
-        while (Running == true && ind < 10)
+        while (Running == true)
         {
             try {
                 clientSocket = serverSocket.accept();
-                //SThread t = new SThread(RoutingTable, clientSocket, ind); // creates a thread with a random port
-                //t.start(); // starts the thread
+                SThread t = new SThread(RoutingTable, clientSocket, ind); // creates a thread with a random port
+                t.start(); // starts the thread
                 ind++; // increments the index
-                System.out.println("| ServerRouter Connection " + ind + " with Client/Server: " + clientSocket.getInetAddress().getHostAddress());
+                System.out.println("ServerRouter connected with Client/Server: " + clientSocket.getInetAddress().getHostAddress());
             }
             catch (IOException e) {
-                System.err.println("| Client/Server failed to connect.");
+                System.err.println("Client/Server failed to connect.");
                 System.exit(1);
             }
         }//end while
